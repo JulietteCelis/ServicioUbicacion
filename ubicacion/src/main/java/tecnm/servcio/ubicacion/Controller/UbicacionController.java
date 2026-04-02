8625696482858df1d8eca60615f7110dc62bd1ea
draca <draca@MSI>
2026-04-02 14:14:57 -0600

    quinto commit

--- ubicacion/src/main/java/tecnm/servcio/ubicacion/Controller/UbicacionController.java
+++ ubicacion/src/main/java/tecnm/servcio/ubicacion/Controller/UbicacionController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,8 @@ import lombok.RequiredArgsConstructor;
 import tecnm.servcio.ubicacion.Dto.UbicacionRequestDTO;
 import tecnm.servcio.ubicacion.Dto.UbicacionResponseDTO;
 import tecnm.servcio.ubicacion.Service.UbicacionService;
+
+@CrossOrigin(origins = "http://localhost:3002") 
 
 @RestController
 @RequestMapping("/api/ubicaciones")
